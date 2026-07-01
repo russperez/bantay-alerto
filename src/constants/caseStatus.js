ef744efc5ada686f596589108e25f3f802e6e854
Dharl Russell Perez <163944465+russperez@users.noreply.github.com>
2026-07-01 11:59:06 +0800

    Added case status and user role

---
+++ src/constants/caseStatus.js
@@ -0,0 +1,12 @@
+export const CASE_STATUS = {
+  ACTIVE: "Active",
+  VERIFIED: "Verified",
+  LOCATED: "Located",
+  RESOLVED: "Resolved",
+};
+
+export const USER_ROLES = {
+  RESIDENT: "resident",
+  VOLUNTEER: "volunteer",
+  BARANGAY_ADMIN: "barangay_admin",
+};
